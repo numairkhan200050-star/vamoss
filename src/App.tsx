e430f6d2b3bfde5bdea7dfd0499336348bf11979
numairkhan200050-star <numairkhan200050@gmail.com>
2026-01-29 23:10:38 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 
-// Core Layout & UI
-// Note: We use try/catch or simple imports to ensure the build doesn't crash
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-
-// Public Pages
-import { HeroSlider } from './components/HeroSlider';
-import { ContentArea } from './components/ContentArea';
-
 function App() {
-  // This is a "Null" version that doesn't use the Router library
-  // It only shows the Home Page to ensure the Build turns GREEN
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <main>
-        <HeroSlider />
-        <ContentArea />
-      </main>
-      <Footer />
-      <ScrollToTop />
+    <div style={{ 
+      height: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      flexDirection: 'column',
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1 style={{ fontSize: '3rem', fontStyle: 'italic' }}>KEVIN 11</h1>
+      <p>Build Status: Testing Environment</p>
     </div>
   );
 }
