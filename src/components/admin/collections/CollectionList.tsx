ac2b19e473c3c9ef9da8a5b2680632bb68fd8295
numairkhan200050-star <numairkhan200050@gmail.com>
2026-02-02 03:58:57 +0500

    Update CollectionList.tsx

--- src/components/admin/collections/CollectionList.tsx
+++ src/components/admin/collections/CollectionList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabase';
 import CollectionForm from './CollectionForm';
 
 export default function CollectionList() {
