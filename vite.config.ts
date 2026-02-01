12753975033cf8e41d785795e7af7e53e40ca8fb
numairkhan200050-star <numairkhan200050@gmail.com>
2026-02-02 04:44:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/vamoss/', // 🔴 MUST MATCH REPO NAME
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
