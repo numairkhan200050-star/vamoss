fcff9e1d9efb7702cd104ef95ac9e4eabaa7ee2f
numairkhan200050-star <numairkhan200050@gmail.com>
2026-02-04 01:11:52 +0500

    Update ProductForm.tsx

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../../lib/supabase';
 import { LoginPortal } from './LoginPortal';
 import CollectionList from './admin/collections/CollectionList';
 import PagesList from './admin/pages/PagesList';
