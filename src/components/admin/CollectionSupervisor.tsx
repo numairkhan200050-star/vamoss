a3509b9b638b28ee25cf4f0652562a9548c91880
numairkhan200050-star <numairkhan200050@gmail.com>
2026-02-04 12:00:03 +0500

    Update CollectionSupervisor.tsx

--- src/components/admin/CollectionSupervisor.tsx
+++ src/components/admin/CollectionSupervisor.tsx
@@ -19,7 +19,7 @@ export const CollectionSupervisor = () => {
       
       {/* 1. THE VIEWER (LIST) */}
       {currentView === 'VIEW_ALL' && (
-        <CollectionList 
+        <CollectionView 
           onAddNew={() => setCurrentView('CREATE_NEW')}
           onEdit={(id) => {
             console.log("Editing Collection:", id);
